c159b1d92088ef22198f0ac7682cd2c65bb601c5
Prangula <Prangula731@gmail.com>
2024-06-06 15:33:56 +0400

    Repository, ViewModel

---
+++ app/src/main/java/com/example/tvshowsapp/repository/MyRepository.kt
@@ -0,0 +1,10 @@
+package com.example.tvshowsapp.repository
+
+import com.example.tvshowsapp.api.ApiService
+import javax.inject.Inject
+
+class MyRepository@Inject constructor(private val apiService: ApiService){
+
+    suspend fun getTvShows() = apiService.getTvShows()
+
+}
---
+++ app/src/main/java/com/example/tvshowsapp/viewmodel/MyViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.tvshowsapp.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tvshowsapp.model.TvShowResponseItem
+import com.example.tvshowsapp.repository.MyRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MyViewModel@Inject constructor(private val myRepository: MyRepository):ViewModel() {
+
+    private val _tvShowResponse = MutableLiveData<List<TvShowResponseItem>>()
+    val tvShowResponse:LiveData<List<TvShowResponseItem>> get() = _tvShowResponse
+
+    init {
+        getAllTvShows()
+    }
+
+
+    private fun getAllTvShows() = viewModelScope.launch{
+
+        myRepository.getTvShows().let { response->
+
+            if(response.isSuccessful){
+                _tvShowResponse.postValue(response.body())
+            }
+        }
+
+    }
+
+
+}
