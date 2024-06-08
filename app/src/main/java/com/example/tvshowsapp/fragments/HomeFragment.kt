b61eb31e0b6cdf3cf88dd642eb56719f6ed14e3d
Prangula <Prangula731@gmail.com>
2024-06-08 12:49:16 +0400

    Change Rv Items Layout

--- app/src/main/java/com/example/tvshowsapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/tvshowsapp/fragments/HomeFragment.kt
@@ -7,6 +7,7 @@ import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tvshowsapp.R
 import com.example.tvshowsapp.adapter.TvShowAdapter
@@ -43,7 +44,7 @@ class HomeFragment:Fragment(R.layout.fragment_home) {
         rv_shows.apply {
 
             adapter = tvShowAdapter
-            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = GridLayoutManager(activity,2,GridLayoutManager.VERTICAL,false)
             setHasFixedSize(true)
         }
 
--- app/src/main/java/com/example/tvshowsapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/tvshowsapp/fragments/SearchFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tvshowsapp.R
 import com.example.tvshowsapp.adapter.SearchTvShowAdapter
@@ -70,7 +71,7 @@ class SearchFragment:Fragment(R.layout.fragment_search) {
         rv_search_shows.apply {
 
             adapter = searchShowAdapter
-            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = GridLayoutManager(activity,2, GridLayoutManager.VERTICAL,false)
             setHasFixedSize(true)
 
         }
